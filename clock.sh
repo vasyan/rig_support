d4895c019e2bb98ad278b6a364375c046f79e633
vasyan <vasilyator@gmail.com>
2018-02-25 21:37:39 +0700

    downvolt for 1 and 2

--- clock.sh
+++ clock.sh
@@ -1,8 +1,8 @@
 #!bin/bash
 
-sudo nvidia-smi -i 0 -pl 125
+sudo nvidia-smi -i 0 -pl 120
 sudo nvidia-smi -i 1 -pl 130
-sudo nvidia-smi -i 2 -pl 130
+sudo nvidia-smi -i 2 -pl 125
 sudo nvidia-smi -i 3 -pl 130
 sudo nvidia-smi -i 4 -pl 130
 
